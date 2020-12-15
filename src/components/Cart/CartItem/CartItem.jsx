6fbf5a1833515d2a4a099f46ee3330ef9d78d8e7
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-12-15 15:28:31 +0200

    adding buttons functionality

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -9,7 +9,7 @@ import {
 
 import useStyles from "./styles";
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
 	const classes = useStyles();
 
 	return (
@@ -20,22 +20,39 @@ const CartItem = ({ item }) => {
 				className={classes.media}
 			/>
 			<CardContent className={classes.cardContent}>
-				<Typography variant="h4">{item.name}</Typography>
+				<Typography variant="h6">{item.name}</Typography>
 				<Typography variant="h5">
 					{item.line_total.formatted_with_symbol}
 				</Typography>
 			</CardContent>
 			<CardActions className={classes.cardActions}>
 				<div className={classes.buttons}>
-					<Button type="button" size="small">
+					<Button
+						type="button"
+						size="small"
+						onClick={() =>
+							onUpdateCartQty(item.id, item.quantity - 1)
+						}
+					>
 						-
 					</Button>
 					<Typography>{item.quantity}</Typography>
-					<Button type="button" size="small">
+					<Button
+						type="button"
+						size="small"
+						onClick={() =>
+							onUpdateCartQty(item.id, item.quantity + 1)
+						}
+					>
 						+
 					</Button>
 				</div>
-				<Button variant="contained" type="button" color="secondary">
+				<Button
+					variant="contained"
+					type="button"
+					color="secondary"
+					onClick={() => onRemoveFromCart(item.id)}
+				>
 					Remove
 				</Button>
 			</CardActions>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,18 +8,22 @@ import {
 	Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
 
 import logo from "../../assets/cornershop.png";
-import useStyles from "./style";
+import useStyles from "./styles";
 
 const Navbar = ({ totalItems }) => {
 	const classes = useStyles();
+	const location = useLocation();
 
 	return (
 		<>
 			<AppBar position="fixed" className={classes.appbar} color="inherit">
 				<Toolbar>
 					<Typography
+						component={Link}
+						to="/"
 						variant="h6"
 						className="classes.title"
 						color="inherit"
@@ -32,16 +36,23 @@ const Navbar = ({ totalItems }) => {
 						/>
 					</Typography>
 					<div className={classes.grow} />
-					<div className={classes.button}>
-						<IconButton
-							aria-label="Show cart items"
-							color="inherit"
-						>
-							<Badge badgeContent={totalItems} color="secondary">
-								<ShoppingCart />
-							</Badge>
-						</IconButton>
-					</div>
+					{location.pathname === "/" && (
+						<div className={classes.button}>
+							<IconButton
+								component={Link}
+								to="/cart"
+								aria-label="Show cart items"
+								color="inherit"
+							>
+								<Badge
+									badgeContent={totalItems}
+									color="secondary"
+								>
+									<ShoppingCart />
+								</Badge>
+							</IconButton>
+						</div>
+					)}
 				</Toolbar>
 			</AppBar>
 		</>
