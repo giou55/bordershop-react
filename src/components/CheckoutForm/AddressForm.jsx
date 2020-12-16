460f6db0dcb31eb509ec714474e67ca76c096f62
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-12-16 19:04:59 +0200

    working on checkout page

---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,46 @@
+import {
+	InputLabel,
+	Select,
+	MenuItem,
+	Button,
+	Grid,
+	Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+
+import FormInput from "./CustomTextField";
+
+const AddressForm = () => {
+	const methods = useForm();
+
+	return (
+		<>
+			<Typography variant="h6" gutterBottom>
+				Shipping Address
+			</Typography>
+			<FormProvider {...methods}>
+				<form onSubmit="">
+					<Grid container spacing={3}>
+						<FormInput
+							required
+							name="firstName"
+							label="First Name"
+						/>
+						<FormInput required name="lastName" label="Last Name" />
+						<FormInput required name="address1" label="Address" />
+						<FormInput required name="email" label="Email" />
+						<FormInput required name="phone" label="Phone Number" />
+						<FormInput required name="city" label="City" />
+						<FormInput
+							required
+							name="zip"
+							label="ZIP / Postal code"
+						/>
+					</Grid>
+				</form>
+			</FormProvider>
+		</>
+	);
+};
+
+export default AddressForm;
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -11,12 +11,18 @@ import {
 } from "@material-ui/core";
 
 import useStyles from "./styles";
+import AddressForm from "../AddressForm";
+import PaymentForm from "../PaymentForm";
 
 const steps = ["Shipping address", "Payment details"];
 
 const Checkout = () => {
 	const [activeStep, setActiveStep] = useState(0);
 	const classes = useStyles();
+
+	const Confirmation = () => <div>Confirmation</div>;
+
+	const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
 
 	return (
 		<>
@@ -26,13 +32,17 @@ const Checkout = () => {
 					<Typography variant="h4" align="center">
 						Checkout
 					</Typography>
-					<Stepper activeStep={0} className={classes.stepper}>
+					<Stepper
+						activeStep={activeStep}
+						className={classes.stepper}
+					>
 						{steps.map((step) => (
 							<Step key={step}>
 								<StepLabel>{step}</StepLabel>
 							</Step>
 						))}
 					</Stepper>
+					{activeStep === steps.length ? <Confirmation /> : <Form />}
 				</Paper>
 			</main>
 		</>
